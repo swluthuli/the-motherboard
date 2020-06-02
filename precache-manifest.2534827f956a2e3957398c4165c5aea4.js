d877a217991f380a5446f6ef2d5522dda2cfd257
swluthuli <swluthuli@gmail.com>
2020-06-03 01:07:27 +0200

    Updates

---
+++ precache-manifest.2534827f956a2e3957398c4165c5aea4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0287513d27a63342ac08dea8ed996167",
+    "url": "/the-motherboard/index.html"
+  },
+  {
+    "revision": "1746e79503e3fe02c5dc",
+    "url": "/the-motherboard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2c185ab386c6360e0642",
+    "url": "/the-motherboard/static/css/main.958fd763.chunk.css"
+  },
+  {
+    "revision": "1746e79503e3fe02c5dc",
+    "url": "/the-motherboard/static/js/2.b4ba04c0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/the-motherboard/static/js/2.b4ba04c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c185ab386c6360e0642",
+    "url": "/the-motherboard/static/js/main.df19f9a8.chunk.js"
+  },
+  {
+    "revision": "0821f60b33b335530796",
+    "url": "/the-motherboard/static/js/runtime-main.db7ced53.js"
+  },
+  {
+    "revision": "97b1db4f3d3edf061542f4a63bfdbfc4",
+    "url": "/the-motherboard/static/media/1.97b1db4f.jpg"
+  },
+  {
+    "revision": "c63962941abc94ce0f1dea94d69470d5",
+    "url": "/the-motherboard/static/media/logo.c6396294.png"
+  }
+]);
